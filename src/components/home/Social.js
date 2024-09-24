fe1dd5c062d5aa9ca788d85a25da370888cbd187
OwinoMichael <owinomichael1400.com>
2024-09-24 19:32:50 -0400

    Home,skills,contact

---
+++ src/components/home/Social.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a href='https://www.instgram.com/' className='home__social-icon' target="_blank" rel="noreferrer">
+        <i className='uil uil-blogger'></i>
+      </a>
+
+      <a href='https://www.google.com/' className='home__social-icon' target="_blank" rel="noreferrer">
+        <i className='uil uil-dribbble'></i>
+      </a>
+
+      <a href='https://www.github.com/' className='home__social-icon' target="_blank" rel="noreferrer">
+        <i className='uil uil-github-alt'></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
