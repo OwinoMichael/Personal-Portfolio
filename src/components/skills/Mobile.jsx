527e2ada49c4da03b07d40fe4e155caba129e480
OwinoMichael <owinomichael1400@gmail.com>
2025-01-07 17:13:09 -0500

    More static updates

---
+++ src/components/skills/Mobile.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Mobile = () => {
+  return (
+    <div className='skills__content'>
+        <h3 className='skills__title'>Mobile Developer</h3>
+
+        <div className='skills__box'>
+            <div className='skills__group'>
+                <div className='skills__data'></div>
+                <i className='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className='skills__name'>Flutter</h3>
+                    <span className='skills__level'>Advanced</span>
+                </div>
+
+                <div className='skills__data'></div>
+                <i className='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className='skills__name'>Android Java</h3>
+                    <span className='skills__level'>Pro</span>
+                </div>
+
+                <div className='skills__data'></div>
+                <i className='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className='skills__name'>React Native</h3>
+                    <span className='skills__level'>Beginner</span>
+                </div>
+
+                
+            </div>
+
+            <div className='skills__group'>
+                
+
+                
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Mobile
